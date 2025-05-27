ce14c055c93e5f5dc1bd0c35641dd1ebd10a1486
Brian <karanjabrian382@gmail.com>
2025-05-27 18:49:02 +0300

    7th task /

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,10 +1,13 @@
+
+
 export interface PillProps {
   title: string;
-  styles?: string;
+  styles?: string; // optional styles for the Pill
 }
+
 export interface ButtonProps {
   title: string;
   size?: "small" | "medium" | "large";
   shape?: "rounded-sm" | "rounded-md" | "rounded-full";
-  styles?: string;
+  styles?: string; // optional styles for the Button
 }
